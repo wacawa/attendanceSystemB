d828d481ff8fd519735f236a175fb558449d40a9
EC2 Default User <ec2-user@ip-172-31-46-139.ap-northeast-1.compute.internal>
2020-03-16 03:09:38 +0000

    Finish login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
+      flash[:success] = "作成に成功しました。"
       redirect_to @user
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  
   root 'static_pages#top'
-  
+    
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 
 end
